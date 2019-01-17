026de1add23485dd1502ff50e889ee57265e4c4c
Hegar Garcia <hegargarcia@gmail.com>
2019-01-16 20:20:10 -0600

    Move manhattan distance to typescript

--- manhattan_distance/index.test.ts
+++ manhattan_distance/index.test.ts
@@ -1,29 +1,34 @@
-const createGrid = require('./index');
+import { createGrid, IGrid } from '.';
 
-let grid;
+let grid: IGrid;
 
-test('Should create a grid', () => {
+test('Should create a grid (3 x 3)', () => {
   grid = createGrid();
 
   expect(grid).toBeTruthy();
+  expect(grid).toHaveProperty('getDistance');
 });
 
-test('Distance from (0, 0) to (0, 0)', () => {
+test('Distance from Point(0, 0) to Point(0, 0)', () => {
   const distance = grid.getDistance({ x: 0, y: 0 }, { x: 0, y: 0 });
+
   expect(distance).toBe(0);
 });
 
-test('Distance from (0, 0) to (3, 3)', () => {
+test('Distance from Point(0, 0) to Point(3, 3)', () => {
   const distance = grid.getDistance({ x: 0, y: 0 }, { x: 3, y: 3 });
+
   expect(distance).toBe(6);
 });
 
-test('Distance from (0, 1) to (2, 3)', () => {
+test('Distance from Point(0, 1) to Point(2, 3)', () => {
   const distance = grid.getDistance({ x: 0, y: 1 }, { x: 2, y: 3 });
+
   expect(distance).toBe(4);
 });
 
-test('Distance from (0, 3) to (3, 0)', () => {
+test('Distance from Point(0, 3) to Point(3, 0)', () => {
   const distance = grid.getDistance({ x: 0, y: 3 }, { x: 3, y: 0 });
+
   expect(distance).toBe(6);
 });
